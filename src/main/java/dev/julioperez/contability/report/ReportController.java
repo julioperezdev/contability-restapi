5740a40aa03e64c2397c51521742132ac4c5711f
Julio Perez Viloria <protobot.dev@gmail.com>
2021-11-17 23:31:38 -0300

    end to end testing ok with report dto using postman

--- src/main/java/dev/julioperez/contability/report/ReportController.java
+++ src/main/java/dev/julioperez/contability/report/ReportController.java
@@ -1,11 +1,16 @@
 package dev.julioperez.contability.report;
 
+import dev.julioperez.contability.shared.RestResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/api/report")
 public class ReportController {
 
     private final ReportBuilder reportBuilder;
@@ -13,5 +18,11 @@ public class ReportController {
     @Autowired
     public ReportController(ReportBuilder reportBuilder) {
         this.reportBuilder = reportBuilder;
+    }
+
+    @GetMapping
+    public RestResponse<List<ReportDTO>> getAllReportDTO(){
+        List<ReportDTO> reportDTOS = reportBuilder.generateReportToAllWorkers();
+        return new RestResponse<>(HttpStatus.FOUND, "Todos los reports", reportDTOS);
     }
 }
